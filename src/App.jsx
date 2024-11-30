8e5d6161f2e2479c870bf8f2cc79ebf459612547
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-11-30 13:18:13 +0100

    Renders components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-function App() {
-  
+import React from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Skills from "./Components/Skills";
+import Timeline from "./Components/Timeline";
+import Project from "./Components/Project";
+import Contact from "./Components/Contact";
 
+function App() {
   return (
-    <>
-      
-    </>
-  )
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow">
+        <About />
+        <Skills />
+        <Timeline />
+        <Project />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
