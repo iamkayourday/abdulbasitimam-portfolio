2ed69185e9b95606a3bd64c0fb2ac117298a5040
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-11-30 16:15:29 +0100

    Update

--- src/Components/AiChatWidget.jsx
+++ src/Components/AiChatWidget.jsx
@@ -4,20 +4,21 @@ const AIChatWidget = () => {
   useEffect(() => {
     // Dynamically load the AI script
     const script = document.createElement('script');
-    script.src = "https://cdn.chatsimple.ai/ai-loader.js";
+    script.src = "https://cdn.chatsimple.ai/chat-bot-loader.js"; // Updated script URL
     script.defer = true;
     document.body.appendChild(script);
   }, []);
 
   return (
-    <co-pilot
-      platform-id="6e75b160-23f0-465b-a3f1-b0b8e7a5e7ee"
-      user-id="b8551be8-7244-4eef-b162-9e44e10b60e2"
-      chatbot-id="0b5ba742-47dd-46f5-ac85-36c1a811d3e9"
-      is-local="false"
+    <chat-bot
+      platform_id="da1d5ebc-37f6-4087-8c65-f3117bc13a2b"
+      user_id="b8551be8-7244-4eef-b162-9e44e10b60e2"
+      chatbot_id="0b5ba742-47dd-46f5-ac85-36c1a811d3e9"
     >
-      <a href="https://www.chatsimple.ai/?utm_source=widget&utm_medium=referral">AI Nav</a>
-    </co-pilot>
+      <a href="https://www.chatsimple.ai/?utm_source=widget&utm_medium=referral">
+        chatsimple
+      </a>
+    </chat-bot>
   );
 };
 
