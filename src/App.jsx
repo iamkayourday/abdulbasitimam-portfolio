d6e8b78f829327cc670d6e2bde664aaf5642d43f
Abdulbasit <abdulbasitkayode@gmail.com>
2025-03-28 10:16:06 +0100

    Update App component: enhance background and text colors for improved dark mode support

--- src/App.jsx
+++ src/App.jsx
@@ -10,21 +10,23 @@ import AIChatWidget from "./Components/AiChatWidget";
 import Testimonial from "./Components/Testimonial";
 import BentoGrid from "./Components/BentoGrid";
 import Services from "./Components/Services";
+// import ThemeSwitcher from "./Components/ThemeSwitcher";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-[#1e1e1f] text-white dark:text-[#1e1e1f] dark:bg-[#F2F2F2]">
       
       <Header />
-      <main className="flex-grow">
+      <main className="flex-grow ">
         <About />
+        {/* <ThemeSwitcher /> */}
         <BentoGrid />
         <Skills />
         <Timeline />
         <Services />
         <Project />
-        <Contact />
         <Testimonial />
+        <Contact />
         <AIChatWidget />
       </main>
       <Footer />
